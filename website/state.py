4522405f84b5a5384a1770d6a14d8948e66cb098
Salim Houari <salim.houari@gmail.com>
2025-10-15 17:42:55 -0400

    fix: Update Supabase client initialization to use get_supabase_client

--- website/state.py
+++ website/state.py
@@ -1,8 +1,8 @@
 import reflex as rx
-from website.utils.supabase_client import create_client
+from website.utils.supabase_client import get_supabase_client
 
 
-supabase = create_client()
+supabase = get_supabase_client()
 class Blog_FormState(rx.State):
     """The state for the blog form."""
     form_data: dict = {}
