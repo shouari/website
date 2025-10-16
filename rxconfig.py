8aa0de4d1c803f56b7dcff291e4e4f2d95bb4e3f
Salim Houari <salim.houari@gmail.com>
2025-10-16 14:54:31 -0400

    fix: Add missing comma in configuration for show_built_with_reflex

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import reflex_enterprise as rxe
 config = rxe.Config(
     app_name="website",
     use_single_port=True,
-    show_built_with_reflex=True
+    show_built_with_reflex=True,
 
     plugins=[
         rx.plugins.SitemapPlugin(),
