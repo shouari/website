887463dbf98d5e15e2db1e1e0f01bb9997fd3deb
Salim Houari <salim.houari@gmail.com>
2025-10-16 14:51:58 -0400

    fix: Update configuration to use rxe.Config instead of rx.Config

--- rxconfig.py
+++ rxconfig.py
@@ -1,8 +1,10 @@
 import reflex as rx
 import reflex_enterprise as rxe
 
-config = rx.Config(
+config = rxe.Config(
     app_name="website",
+    use_single_port=True,
+    show_built_with_reflex=True
 
     plugins=[
         rx.plugins.SitemapPlugin(),
