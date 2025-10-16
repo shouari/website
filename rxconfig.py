cd23cb475ef556feea46833dbf9a716268e1dc68
Salim Houari <salim.houari@gmail.com>
2025-10-16 14:38:41 -0400

    fix: Update configuration to set show_built_with_reflex to False

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,7 @@ import reflex_enterprise as rxe
 config = rxe.Config(
     app_name="website",
     use_single_port=True,
+    show_built_with_reflex=False,
 
     plugins=[
         rx.plugins.SitemapPlugin(),
